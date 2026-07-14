58a30fe53b7ac0e347e89b2aac5dd3ca24b5c1b8
charasucharka <charasucharochka@gmail.com>
2026-07-14 14:01:13 +0300

    now it will build, promise

--- .github/scripts/flutter-dart-defines.ps1
+++ .github/scripts/flutter-dart-defines.ps1
@@ -1,8 +1,14 @@
 $ErrorActionPreference = 'Stop'
 
-if ([string]::IsNullOrEmpty($env:PVZ2C_ENCRYPTION_KEY)) {
-    Write-Error 'PVZ2C_ENCRYPTION_KEY must be set for release builds.'
-    exit 1
+$key = $env:PVZ2C_ENCRYPTION_KEY
+if ([string]::IsNullOrEmpty($key)) {
+    if ($env:GITHUB_EVENT_NAME -eq 'pull_request') {
+        Write-Host "Warning: PVZ2C_ENCRYPTION_KEY is missing. Using a dummy key for Pull Request build." -ForegroundColor Yellow
+        $key = "DUMMY_KEY_FOR_PR_BUILDS"
+    } else {
+        Write-Error 'PVZ2C_ENCRYPTION_KEY must be set for release builds.'
+        exit 1
+    }
 }
 
-Write-Output "--dart-define=PVZ2C_ENCRYPTION_KEY=$($env:PVZ2C_ENCRYPTION_KEY)"
+Write-Output "--dart-define=PVZ2C_ENCRYPTION_KEY=$key"
