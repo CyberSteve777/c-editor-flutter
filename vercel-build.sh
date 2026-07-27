a9439400f58944eee40790dd4ee89f63beb12a14
CyberSteve777 <69720241+cybersteve777@users.noreply.github.com>
2026-07-27 15:08:25 +0300

    add back -O0 build flag

--- vercel-build.sh
+++ vercel-build.sh
@@ -13,4 +13,4 @@ flutter config --enable-web
 flutter precache --web
 flutter pub get
 DART_DEFINES="$(bash .github/scripts/flutter-dart-defines.sh)"
-flutter build web --release --wasm ${DART_DEFINES}
+flutter build web --release -O0 --wasm ${DART_DEFINES}
